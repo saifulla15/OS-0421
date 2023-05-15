905ddcda798bc9cade8c1537802cf4f05cebb369
saifulla shaik <104437948+saifulla15@users.noreply.github.com>
2023-05-15 12:34:18 +0530

    16 firstfit.c

--- 16 firstfit.c
+++ 16 firstfit.c
@@ -1,64 +1,57 @@
-// C implementation of First - Fit algorithm
-#include<stdio.h>
+#include <stdio.h>
 
-// Function to allocate memory to
-// blocks as per First fit algorithm
-void firstFit(int blockSize[], int m, int processSize[], int n)
-{
-	int i, j;
-	// Stores block id of the
-	// block allocated to a process
-	int allocation[n];
+#define MAX_MEM_SIZE 1000  // maximum size of memory
+#define MAX_PROC_SIZE 100  // maximum size of a process
 
-	// Initially no block is assigned to any process
-	for(i = 0; i < n; i++)
-	{
-		allocation[i] = -1;
-	}
+int main() {
+    int mem_size, n;
+    int mem_blocks[MAX_MEM_SIZE], proc_size[MAX_PROC_SIZE], proc_alloc[MAX_PROC_SIZE];
+    int i, j;
 
-	// pick each process and find suitable blocks
-	// according to its size ad assign to it
-	for (i = 0; i < n; i++)	 //here, n -> number of processes
-	{
-		for (j = 0; j < m; j++)	 //here, m -> number of blocks
-		{
-			if (blockSize[j] >= processSize[i])
-			{
-				// allocating block j to the ith process
-				allocation[i] = j;
+    // Get user input for memory size and number of processes
+    printf("Enter the size of the memory: ");
+    scanf("%d", &mem_size);
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
 
-				// Reduce available memory in this block.
-				blockSize[j] -= processSize[i];
+    // Initialize memory blocks to 0 (i.e. unallocated)
+    for (i = 0; i < mem_size; i++) {
+        mem_blocks[i] = 0;
+    }
 
-				break; //go to the next process in the queue
-			}
-		}
-	}
+    // Get user input for process sizes
+    printf("Enter the size of each process:\n");
+    for (i = 0; i < n; i++) {
+        printf("Process %d: ", i + 1);
+        scanf("%d", &proc_size[i]);
+    }
 
-	printf("\nProcess No.\tProcess Size\tBlock no.\n");
-	for (int i = 0; i < n; i++)
-	{
-		printf(" %i\t\t\t", i+1);
-		printf("%i\t\t\t\t", processSize[i]);
-		if (allocation[i] != -1)
-			printf("%i", allocation[i] + 1);
-		else
-			printf("Not Allocated");
-		printf("\n");
-	}
-}
+    // Allocate memory to each process using First Fit algorithm
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < mem_size; j++) {
+            if (mem_blocks[j] == 0 && j + proc_size[i] <= mem_size) {
+                // This memory block is free and large enough to allocate the process
+                int k;
+                for (k = j; k < j + proc_size[i]; k++) {
+                    mem_blocks[k] = i + 1;  // mark the memory block as allocated to process i+1
+                }
+                proc_alloc[i] = j;  // record the starting position of the allocated memory block
+                break;
+            }
+        }
+        if (j == mem_size) {
+            // No free memory block is large enough to allocate the process
+            printf("Error: Not enough memory to allocate Process %d\n", i + 1);
+            return 1;
+        }
+    }
 
-// Driver code
-int main()
-{
-	int m; //number of blocks in the memory
-	int n; //number of processes in the input queue
-	int blockSize[] = {100, 500, 200, 300, 600};
-	int processSize[] = {212, 417, 112, 426};
-	m = sizeof(blockSize) / sizeof(blockSize[0]);
-	n = sizeof(processSize) / sizeof(processSize[0]);
+    // Print the allocation table
+    printf("Allocation Table:\n");
+    printf("Process\tSize\tStart Address\tEnd Address\n");
+    for (i = 0; i < n; i++) {
+        printf("%d\t%d\t%d\t\t%d\n", i + 1, proc_size[i], proc_alloc[i], proc_alloc[i] + proc_size[i] - 1);
+    }
 
-	firstFit(blockSize, m, processSize, n);
-
-	return 0 ;
+    return 0;
 }
